9a83c6a259f3f935ef9b66ad4c9b7c1d1937c0c3
Aaron J Harvey <harvey.aaronj@gmail.com>
2020-03-24 17:33:52 +0000

    Automatically backed up by Learn

--- lib/example.rb
+++ lib/example.rb
@@ -11,19 +11,3 @@ puts phrase
 puts phrase
 puts phrase
 puts phrase
-
-def say_hello_ten_times
-  phrase = "Hello World!"
-  puts phrase
-  puts phrase
-  puts phrase
-  puts phrase
-  puts phrase
-  puts phrase
-  puts phrase
-  puts phrase
-  puts phrase
-  puts phrase
-  puts phrase
-end
-say_hello_ten_times
